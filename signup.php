d481e08edbb63a3dc67263f7841adc3ebb7a8b7f
Roman Meredith <romanmeredith@gmail.com>
2019-12-09 20:19:15 -0700

    initial commit

---
+++ signup.php
@@ -0,0 +1,27 @@
+<?php
+    require "header.php";
+?>
+    <main>
+        <h1>Signup</h1>
+        <?php
+            if(isset($_GET['error'])){
+                if($_GET['error']=="emptyfields"){
+                    echo '<p>fill in all fields!</p>';
+                }
+                
+            }
+            else if($_GET['signup']=='success'){
+                echo '<p>signup success!</p>';
+            }
+        ?>
+        <form action="includes/signup.inc.php" method="post">
+            <input type="text" name="uid" placeholder="Username" >
+            <input type="text" name="email" placeholder="E-mail">
+            <input type="password" name="pwd" placeholder="Password">
+            <input type="password" name="pwd-repeat" placeholder="Repeat Password">
+            <button type="submit" name="signup-submit">Signup</button>
+        </form>
+    </main>
+<?php
+    require "footer.php";
+?>
